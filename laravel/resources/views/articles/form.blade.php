46b47c25e68f1a442cffb52510ec807f3bfa42b4
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-11-10 20:29:40 +0900

    Add the functions to edit, update and delete articles.

--- laravel/resources/views/articles/form.blade.php
+++ laravel/resources/views/articles/form.blade.php
@@ -1,9 +1,9 @@
 @csrf
 <div class="md-form">
     <label>タイトル</label>
-    <input type="text" name="title" class="form-control" required value="{{ old('title') }}">
+    <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
 </div>
 <div class="form-group">
     <label></label>
-    <textarea name="body" rows="16" class="form-control" placeholder="本文" required>{{ old('body') }}</textarea>
+    <textarea name="body" rows="16" class="form-control" placeholder="本文" required>{{ $article->body ?? old('body') }}</textarea>
 </div>
