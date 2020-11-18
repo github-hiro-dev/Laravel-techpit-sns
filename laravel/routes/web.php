4b1b8dd03bf5a42e95bb22b0fd0ad19e81dbbd61
Hiroaki Nishiuchi <nishiuchi.h.0414@gmail.com>
2020-11-18 19:36:47 +0900

    Add google login and user registration.

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,12 +11,18 @@
 |
 */
 
+// ログイン
 Auth::routes();
-
+// Googleログイン
 Route::prefix('login')->name('login.')->group(function(){
     Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+    Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('{provider}.callback');
 });
-
+Route::prefix('register')->name('register.')->group(function(){
+    Route::get('/{provider}', 'Auth\RegisterController@showProviderUserRegistrationForm')->name('{provider}');
+    Route::post('/{provider}', 'Auth\RegisterController@registerProviderUser')->name('{provider}');
+});
+// 記事
 Route::get('/','ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']);
@@ -24,9 +30,9 @@ Route::prefix('articles')->name('articles.')->group(function(){
     Route::put('/{article}/like', 'ArticleController@like')->name('like')->middleware('auth');
     Route::delete('/{article}/like', 'ArticleController@unlike')->name('unlike')->middleware('auth');
 });
-
+// タグ
 Route::get('/tag/{name}', 'TagController@show')->name('tags.show');
-
+// いいね、フォロー
 Route::prefix('users')->name('users.')->group(function(){
     Route::get('/{name}', 'UserController@show')->name('show');
     Route::get('/{name}/likes', 'UserController@likes')->name('likes');
